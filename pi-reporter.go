8000aa2204c6c58f87649e5329f616bd1eb05fed
dpinato <dvdpnt@hotmail.it>
2021-03-03 15:59:04 +0000

    Added support for input arguments

--- pi-reporter.go
+++ pi-reporter.go
@@ -13,10 +13,17 @@ import (
 
 const LogFilePath = "/var/log/pi-reporter.log"
 
-const InfluxDBHost = "192.168.128.200"
-const InfluxDBPort = "8086"
-const InfluxDBNameProd = "pi_reporter"
-const InfluxDBNameDev = "pi_reporter_dev"
+// SupportedArgs:
+// --env: Indicates the environment type, i.e. dev or prod
+// --influxhost: IP address of the host running InfluxDB
+var SupportedArgs = []string{"--env", "--influxhost"}
+
+// constants for InfluxDB connection
+const (
+	InfluxDBPort     = "8086"
+	InfluxDBNameProd = "pi_reporter_prod"
+	InfluxDBNameDev  = "pi_reporter_dev"
+)
 
 func main() {
 	// open log file to append
@@ -28,29 +35,48 @@ func main() {
 	defer f.Close()
 	mw := io.MultiWriter(os.Stdout, f)
 	log.SetOutput(mw)
-
 	log.Printf("pi-reporter is starting ...\n")
 
+	// check input arguments
+	args := parseCmdArgs(os.Args[1:])
+	ok := validateCmdArgs(args)
+	if !ok || len(args) < 2 {
+		log.Fatalf("Bad or missing command arguments, %v\n", args)
+	}
+	log.Println(args)
+
+	// initialise things for the environment selected
+	var influxDBName string
+	switch args["--env"] {
+	case "dev":
+		influxDBName = InfluxDBNameDev
+	case "prod":
+		influxDBName = InfluxDBNameProd
+	default:
+		log.Fatalln("Bad environment selected, terminating ...")
+	}
+
 	// connect to InfluxDB
-	c, err := influxDBClient(InfluxDBHost, InfluxDBPort)
+	influxDBHost := args["--influxhost"]
+	c, err := influxDBClient(influxDBHost, InfluxDBPort)
 	if err != nil {
 		log.Println("Error creating InfluxDB Client: ", err.Error())
 	}
 	defer c.Close()
-	log.Printf("Connected to DB %s:%s\n", InfluxDBHost, InfluxDBPort)
+	log.Printf("Connected to DB %s:%s\n", influxDBHost, InfluxDBPort)
 
 	// start reporting
 	var wg sync.WaitGroup
 	wg.Add(1)
 	go func(wg *sync.WaitGroup) {
 		defer wg.Done()
-		modules.ReportCPUUsage(InfluxDBNameDev, c)
+		modules.ReportCPUUsage(influxDBName, c)
 	}(&wg)
 
 	wg.Add(1)
 	go func(wg *sync.WaitGroup) {
 		defer wg.Done()
-		modules.ReportNetworkStats(InfluxDBNameDev, c)
+		modules.ReportNetworkStats(influxDBName, c)
 	}(&wg)
 
 	wg.Wait()
@@ -63,4 +89,36 @@ func influxDBClient(host, port string) (client.Client, error) {
 		Addr: "http://" + host + ":" + port,
 	})
 	return c, err
+}
+
+func parseCmdArgs(args []string) map[string]string {
+	// a key will start with --, the value should be right after the key
+	output := make(map[string]string)
+	if len(args) != 0 && len(args)%2 == 0 {
+		for i := 0; i < len(args); i += 2 {
+			output[args[i]] = args[i+1]
+		}
+	}
+
+	return output
+}
+
+func validateCmdArgs(args map[string]string) bool {
+	// return true if the arguments provided are within the list of supported arguments
+	for k, v := range args {
+		var found bool
+		for _, elem := range SupportedArgs {
+			if k == elem && v != "" {
+				found = true
+				continue
+			}
+		}
+
+		if !found {
+			// not in list of supported arguments
+			return false
+		}
+	}
+
+	return true
 }
